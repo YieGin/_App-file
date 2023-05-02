6e384cc91ab2af7634b4ba5ce12ecebd36672638
Yie <pumasami7@gmail.col>
2023-05-02 05:46:00 +0100

    added project

--- test/src/components/Header.js
+++ test/src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <div className="bg-red-50 h-20 flex text-[30px] items-center justify-center">
-      <p>Header</p>
+      <p>Headerdsqsqsqsqsqsqsqsqsqsqsq</p>
     </div>
   );
 }
